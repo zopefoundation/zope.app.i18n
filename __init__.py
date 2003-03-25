99ece3c981b53aaf79539eb7a34401f2dfb24481
Jim Fulton <jim@zope.com>
2003-03-25 18:47:13 +0000

    Provide a preconfigured message id factory for import as _.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,22 @@
+##############################################################################
 #
-# This file is necessary to make this directory a package.
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Customization of zope.i18n for the Zope application server
+
+$Id: __init__.py,v 1.3 2003/03/25 18:47:13 jim Exp $
+"""
+
+from zope.i18n import MessageIDFactory
+
+# Import _ to use to create message ids in the zope domain
+ZopeMessageIDFactory = MessageIDFactory('zope')
