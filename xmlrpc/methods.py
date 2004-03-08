2f1219736e4aa28724bd35cb47d2e134ee2639f5
Stephan Richter <stephan.richter@gmail.com>
2004-03-08 23:34:51 +0000

    Moved local translation domain implementation to zope.app.i18n.

---
+++ xmlrpc/methods.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Translation Domain XML-RPC Methods 
+
+$Id: methods.py,v 1.1 2004/03/08 23:34:49 srichter Exp $
+"""
+from zope.proxy import removeAllProxies
+from zope.publisher.xmlrpc import XMLRPCView
+
+
+class Methods(XMLRPCView):
+
+    def getAllLanguages(self):
+        return self.context.getAllLanguages()
+
+    def getMessagesFor(self, languages):
+        messages = []
+        for msg in self.context.getMessages():
+            if msg['language'] in languages:
+                messages.append(removeAllProxies(msg))
+
+        return messages
